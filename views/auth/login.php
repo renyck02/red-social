58f88155e4f5e067c3520224deaff5d085be0b77
renyck02 <145300602+renyck02@users.noreply.github.com>
2024-04-05 13:36:25 -0700

    login hecho

--- views/auth/login.php
+++ views/auth/login.php
@@ -1,13 +1,22 @@
-<form method="POST" >
-    <div class="login__campo">
-        <label for="email">Correo</label>
-        <input type="email" id="email" name="email">
-    </div>
-    <div class="login__campo">
-        <label for="password">Password</label>
-        <input type="password" id="password" name="password">
-    </div>
 
-    <input type="submit">
+<main class="contenedor-sm">
+    <form method="POST" class="formulario-auth">
+        <h1 class="formulario-auth__titulo">Login</h1>
+        <div class="formulario-auth__campo">
+            <label for="email" class="formulario-auth__campo--label">Correo</label>
+            <input type="email" id="email" name="email" class="formulario-auth__campo--input">
+        </div>
+        <div class="formulario-auth__campo">
+            <label for="password" class="formulario-auth__campo--label">Password</label>
+            <input type="password" id="password" name="password" class="formulario-auth__campo--input">
+        </div>
 
-</form>
+        <input type="submit" class="formulario-auth__submit" value="Sign in">
+    </form>
+    <div class="acciones">
+        <a href="/olvide" class="acciones__enlace">¿Olvidaste tu contraseña?</a>
+        <a href="/registrar" class="acciones__enlace">¿No tienes una cuenta?</a>
+    </div>
+</main>
+
+
