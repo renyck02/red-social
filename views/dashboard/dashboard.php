08a654d0dccae25cabc70ace88bf31b1e3c1d3f6
renyck02 <145300602+renyck02@users.noreply.github.com>
2024-04-12 13:24:07 -0700

    test

---
+++ views/dashboard/dashboard.php
@@ -0,0 +1,42 @@
+<div class="dashboard">
+    <div class="dashboard__campos">
+        <div class="dashboard__campo">
+            <div class="dashboard__campo-imagen">
+                <picture>
+                    <source src="/build/img/hogar.webp" type="image/webp">
+                    <source src="/build/img/hogar.avif" type="image/avif">
+                    <source src="/build/img/hogar.png" type="image/png">
+                    <img src="/build/img/hogar.png" alt="home">
+                </picture>
+            </div>
+            <p class="dashboard__campo--texto">Home</p>
+        </div>
+
+        <div class="dashboard__campo">
+            <div class="dashboard__campo-imagen">
+                <picture>
+                    <source src="/build/img/groups.webp" type="image/webp">
+                    <source src="/build/img/groups.avif" type="image/avif">
+                    <source src="/build/img/groups.png" type="image/png">
+                    <img src="/build/img/groups.png" alt="home">
+                </picture>
+            </div>
+            <p class="dashboard__campo--texto">Groups</p>
+        </div>
+
+        <div class="dashboard__campo">
+            <div class="dashboard__campo-imagen">
+                <picture>
+                    <source src="/build/img/chats.webp" type="image/webp">
+                    <source src="/build/img/chats.avif" type="image/avif">
+                    <source src="/build/img/chats.png" type="image/png">
+                    <img src="/build/img/chats.png" alt="home">
+                </picture>
+            </div>
+            <p class="dashboard__campo--texto">Chats</p>
+        </div>
+
+        
+    </div>
+    <div class="dashboard__user"></div>
+</div>
