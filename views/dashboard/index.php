7aceac33596556b9f126bb421714489c021c0691
renyck02 <145300602+renyck02@users.noreply.github.com>
2024-04-12 15:21:15 -0700

    test

--- views/dashboard/index.php
+++ views/dashboard/index.php
@@ -1,12 +1,52 @@
 <?php include_once "dashboard.php"; ?>
 
 <div class="contenedor contenedor-app">
-    <div class="amigos">
+    <div class="contenedor-izquierda">
+        <div class="amigos">
         <h3 class="amigos__texto">
             Amigos
         </h3>
         <div class="amigo">
-            
+            <div class="amigo__imagen"></div>
+            <p class="amigo__nombre">Renéitor Pro</p>
+        </div>
+        <div class="amigo">
+            <div class="amigo__imagen"></div>
+            <p class="amigo__nombre">Davo luis</p>
+        </div>
+        <div class="amigo">
+            <div class="amigo__imagen"></div>
+            <p class="amigo__nombre">Tonoto yahel</p>
         </div>
     </div>
+
+    <div class="chats-recientes">
+        <h3 class="chats-recientes__texto">Chats Recientes</h3>
+        <div class="chat-reciente">
+            <div class="chat-reciente__imagen"></div>
+            <p class="chat-reciente__nombre">Willy Rex</p>
+        </div>
+        <div class="chat-reciente">
+            <div class="chat-reciente__imagen"></div>
+            <p class="chat-reciente__nombre">Yahel Tonoto</p>
+        </div>
+        <div class="chat-reciente">
+            <div class="chat-reciente__imagen"></div>
+            <p class="chat-reciente__nombre">Don Macario</p>
+        </div>
+    </div>
+    </div>
+
+    <div class="contenedor-derecha">
+        <div class="publicar">
+            <h3 class="publicar__texto">Publicar</h3>
+            <div class="publicar__input">
+                <div class="publicar__imagen"></div>
+                <p class="publicar__texto">Di algo...</p>
+            </div>
+        </div>
+    </div>
+    
+
+
 </div>
